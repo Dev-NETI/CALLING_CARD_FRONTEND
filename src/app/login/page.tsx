94883db29a056091822776426fa8cba1440affc7
Meliodas1827 <dasmelio564@gmail.com>
2026-07-02 10:52:44 +0800

    change app name in login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -99,7 +99,7 @@ export default function LoginPage() {
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.6, delay: 0.4 }}
             >
-              NETI Business Card App
+              E-Business Card
             </motion.h1>
 
             <motion.p
